ddb24fbcf95f31713e9c59727af4f45f62839be4
OnurAlan20 <onuralann20@gmail.com>
2024-05-11 10:02:22 +0300

    Initial Commit

---
+++ News Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Application
+//
+//  Created by Onur Alan on 11.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
